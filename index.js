cea89e60be228ce1e3a5c7763781ff0ba1dfa747
AnisMeghani <164073707+AnisMeghani@users.noreply.github.com>
2024-03-23 00:46:35 +0500

    created number guessing game

---
+++ index.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from 'inquirer';
+// 1) computer will generate a random number
+// 2) user input for guessing number
+//compare user input with computer generated number and show result
+const randomNumber = Math.floor(Math.random() * 6 + 1); // dice bana dys ;)
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1 to 6",
+    },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("Congratulations! You guessed right number");
+}
+else {
+    console.log("You guessed the wrong number");
+}
